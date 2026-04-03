0046534d5e267969a03e7f9b713995e1741cd7a3
Marfael34 <raphaelm0085@gmail.com>
2026-04-03 16:22:16 +0200

    add api constant

---
+++ src/constants/apiConstant.js
@@ -0,0 +1,15 @@
+//======================
+// CONSTANTES DE L'API
+//======================
+// ce fichier centralisent toutes les URL de l'API Symfony 
+// l'avantage: Modifier l'URL de base en un seul endroit 
+
+// URL racine du serveur backend
+export const API_ROOT = 'http://localhost:8000';
+
+// l'URL de base pour l'API Plateform 
+export const API_URL = `${API_ROOT}/api`;
+
+// ==============================
+// URLS DES RESSOURCES STATIQUES
+// ==============================
