40d96a7a308807a55daede15c344252ad2a36375
Marfael34 <raphaelm0085@gmail.com>
2026-04-07 11:51:24 +0200

    add first  screens

--- src/constants/apiConstant.js
+++ src/constants/apiConstant.js
@@ -5,7 +5,7 @@
 // l'avantage: Modifier l'URL de base en un seul endroit 
 
 // URL racine du serveur backend
-export const API_ROOT = 'http://localhost:8000';
+export const API_ROOT = 'http://localhost:8087';
 
 // l'URL de base pour l'API Plateform 
 export const API_URL = `${API_ROOT}/api`;
@@ -13,3 +13,10 @@ export const API_URL = `${API_ROOT}/api`;
 // ==============================
 // URLS DES RESSOURCES STATIQUES
 // ==============================
+
+// Image générales  (logo, ect)
+export const IMAGE_URL = `${API_ROOT}/images`;
+
+// Avatars des utilisateur 
+export const AVATAR_URL = `${IMAGE_URL}/avatars`;
+
