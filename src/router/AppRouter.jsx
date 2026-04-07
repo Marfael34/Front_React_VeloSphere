ba7ebc7a1d140c7ac6eaff07c5f255f6a4718091
Marfael34 <raphaelm0085@gmail.com>
2026-04-07 13:50:56 +0200

    add app router

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { RouterProvider } from 'react-router-dom'
+import OfflineRouter from './OfflineRouter'
+
+const AppRouter = () => {
+  return (
+    
+    // TODO: prevoir context d'authentification et de session pour savoir quelle routeur choisir
+
+    <RouterProvider router={OfflineRouter} />
+  )
+}
+
+export default AppRouter
--- src/screens/OfflineScreens/HomeOffline.jsx
+++ src/screens/OfflineScreens/HomeOffline.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const HomeOffline = () => {
   return (
-    <div>HomeOffline</div>
+    <>
+        <div>HomeOffline</div>
+        <Outlet/>
+    </>
+    
   )
 }
 
