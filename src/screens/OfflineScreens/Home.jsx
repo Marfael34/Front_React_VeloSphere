c4fdf16abf573a9f2f5dc3628ab5885750e69bd0
Marfael34 <raphaelm0085@gmail.com>
2026-04-08 20:58:58 +0200

    add paragraphe in Home (a finir)

--- src/screens/OfflineScreens/Home.jsx
+++ src/screens/OfflineScreens/Home.jsx
@@ -3,11 +3,12 @@ import React from 'react'
 const HomeOffline = () => {
   return (
     <>
-      <div className="flex flex-col items-center h-screen px-4 md:px-6 pt-5 bg-dark-nigth-blue bg-opacity-50">
-        <div className="flex justify-center items-center">
+      <div className="flex flex-col items-center h-screen px-4 md:px-6 pt-5 bg-opacity-50">
+        <div className="flex flex-col gap-5 justify-center items-center shadow-2xl">
           <h2 className=" text-2xl md:text-6xl text-white font-bold">Bienvenue sur LaRoueLibre</h2>
-          <p className="">
-            
+          <h3 className=" text-lg md:text-xl text-white font-bold">A quoi sert LaRoueLibre ?</h3>
+          <p className="text-md ">
+            Plus qu'une simple boutique de vélos et de pièces détachées, LaRoueLibre est votre guide pour trouver des itinéraires magnifiques à parcourir à deux roues.
           </p>
         </div>
       </div>
