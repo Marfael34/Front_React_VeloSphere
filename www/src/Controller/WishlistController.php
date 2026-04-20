69f148f1e0553d7894fe94a1a797e45e1439d857
Marfael34 <raphaelm0085@gmail.com>
2026-04-20 16:46:41 +0200

    Fix a bug on the wishlist - add récupération of createdAt

--- www/src/Controller/WishlistController.php
+++ www/src/Controller/WishlistController.php
@@ -13,7 +13,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-// Vérifie que cette route de base est bien là
 #[Route('/api/wishlist', name: 'api_wishlist_')]
 class WishlistController extends AbstractController
 {
@@ -22,7 +21,6 @@ class WishlistController extends AbstractController
     public function getUserWishlist(WishlistRepository $wishlistRepository): JsonResponse
     {
         $user = $this->getUser();
-        // Récupère uniquement les entrées de la table wishlist pour cet utilisateur
         $wishlistItems = $wishlistRepository->findBy(['user' => $user]);
 
         $data = array_map(function(Wishlist $item) {
@@ -34,7 +32,9 @@ class WishlistController extends AbstractController
                 'placeDescription' => $place->getDescription(),
                 'placeImg' => $place->getPath(),
                 'placeDifficulty' => $place->getDifficulty(),
+                'placeId' => $item->getPlace()->getId(),
                 'etatId' => $item->getEtat()->getId(),
+                'createdAt' => $item->getCreatedAt() ? $item->getCreatedAt()->format('d/m/Y H:i') : null,
             ];
         }, $wishlistItems);
 
@@ -55,10 +55,17 @@ class WishlistController extends AbstractController
         }
 
         $etat = $etatRepo->findOneBy(['label' => 'Favoris']);
+        
+        if (!$etat) {
+            return new JsonResponse(['error' => 'État Favoris non trouvé'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
         $new = new Wishlist();
         $new->setUser($user);
         $new->setPlace($place);
         $new->setEtat($etat);
+        // La date est généralement gérée dans le constructeur de l'entité Wishlist
+        
         $em->persist($new);
         $em->flush();
 
